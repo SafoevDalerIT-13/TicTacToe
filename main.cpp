ce97308a260fe0f1da630ba47b739ebcd79ab2c1
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2024-12-02 17:21:59 +0300

    Добавлен русский язык

--- main.cpp
+++ main.cpp
@@ -38,37 +38,39 @@ bool makeMove(vector<vector<char>>& board, int row, int col, char player) {
     return false;
 }
 // Основная функция игры 
-int main() { 
-    vector<vector<char>> board(3, vector<char>(3, ' ')); 
-    char currentPlayer = 'X'; 
-    int moves = 0; 
- 
-    while (true) { 
-        printBoard(board); 
-        int row, col; 
-        cout << "Игрок " << currentPlayer << ", введите строку и столбец (0-2): "; 
-        cin >> row >> col; 
- 
-        // Обработка хода 
-        if (!makeMove(board, row, col, currentPlayer)) { 
-            cout << "Неверный ход. Попробуйте снова." << endl; 
-            continue; 
-        } 
- 
-        moves++; 
-        if (checkWin(board, currentPlayer)) { 
-            printBoard(board); 
-            cout << "Игрок " << currentPlayer << " победил!" << endl; 
-            break; 
-        } else if (moves == 9) { 
-            printBoard(board); 
-            cout << "Игра закончилась вничью!" << endl; 
-            break; 
-        } 
- 
-        // Смена игрока 
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X'; 
-    } 
- 
-    return 0; 
+int main() {
+    setlocale(LC_ALL, "RU");
+    vector<vector<char>> board(3, vector<char>(3, ' '));
+    char currentPlayer = 'X';
+    int moves = 0;
+
+    while (true) {
+        printBoard(board);
+        int row, col;
+        cout << "Игрок " << currentPlayer << ", введите строку и столбец (0-2): ";
+        cin >> row >> col;
+
+        // Обработка хода
+        if (!makeMove(board, row, col, currentPlayer)) {
+            cout << "Неверный ход. Попробуйте снова." << endl;
+            continue;
+        }
+
+        moves++;
+        if (checkWin(board, currentPlayer)) {
+            printBoard(board);
+            cout << "Игрок " << currentPlayer << " победил!" << endl;
+            break;
+        }
+        else if (moves == 9) {
+            printBoard(board);
+            cout << "Игра закончилась вничью!" << endl;
+            break;
+        }
+
+        // Смена игрока
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    return 0;
 }
