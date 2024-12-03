ae3341a2aa8ccc498589f31c47f2ace103a28a73
SafoevDalerIT-13 <dsafoev40@gmail.com>
2024-12-03 08:29:15 +0300

    Добавил свой фрагмент файла

---
+++ Daler.cpp
@@ -0,0 +1,25 @@
+// Функция для проверки победы 
+bool checkWin(const vector<vector<char>>& board, char player) {
+    // Проверка строк и столбцов 
+    for (int i = 0; i < 3; i++) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
+            return true;
+        }
+    }
+    // Проверка диагоналей 
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return true;
+    }
+    return false;
+}
+
+// Функция для обработки хода 
+bool makeMove(vector<vector<char>>& board, int row, int col, char player) {
+    if (board[row][col] == ' ') {
+        board[row][col] = player;
+        return true;
+    }
+    return false;
+}
