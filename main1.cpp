fa2480f694cd5198048aba4c1dc4c8046a4e7129
SafoevDalerIT-13 <dsafoev40@gmail.com>
2024-12-03 09:01:36 +0300

    Добавлен файл с основной программой

---
+++ main1.cpp
@@ -0,0 +1,42 @@
+#include <iostream> 
+#include <vector> 
+#include "TicTacToe.h" // Включаем заголовочный файл 
+
+using namespace std;
+
+// Основная функция игры 
+int main() {
+    vector<vector<char>> board(3, vector<char>(3, ' '));
+    char currentPlayer = 'X';
+    int moves = 0;
+
+    while (true) {
+        printBoard(board);
+        int row, col;
+        cout << "Игрок " << currentPlayer << ", введите строку и столбец (0-2): ";
+        cin >> row >> col;
+
+        // Обработка хода 
+        if (!makeMove(board, row, col, currentPlayer)) {
+            cout << "Неверный ход. Попробуйте снова." << endl;
+            continue;
+        }
+
+        moves++;
+        if (checkWin(board, currentPlayer)) {
+            printBoard(board);
+            cout << "Игрок " << currentPlayer << " победил!" << endl;
+            break;
+        }
+        else if (moves == 9) {
+            printBoard(board);
+            cout << "Игра закончилась вничью!" << endl;
+            break;
+        }
+
+        // Смена игрока 
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    return 0;
+}
