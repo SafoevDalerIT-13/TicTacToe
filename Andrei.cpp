4512b2c1de9b3c88076f0190d4f0b8c8c0a1d2f1
AndreiPanchenko <andrei.panchenko.2006@gmail.com>
2024-12-03 10:54:07 +0500

    Добавлен свой фрагмент кода

---
+++ Andrei.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <vector> 
+ 
+using namespace std; 
+ 
+// Функция для вывода игрового поля 
+void printBoard(const vector<vector<char>>& board) { 
+    for (const auto& row : board) { 
+        for (const auto& cell : row) { 
+            cout << cell << " "; 
+        } 
+        cout << endl; 
+    } 
+}
